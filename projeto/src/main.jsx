58ee16acebd312147a59c9925282958a6ec097fd
GustavoBravin <gugarbravin@gmail.com>
2023-09-01 13:59:42 -0400

    dia 01/09/2023
    kkkk

---
+++ projeto/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from './Home/Home';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+<React.StrictMode>
+<BrowserRouter>
+<Routes>
+<Route path="/" element = {<Home/>}></Route>
+</Routes>
+</BrowserRouter>
+</React.StrictMode>
+)
